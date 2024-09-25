57c1aa58329a1eed186c37eb85f4063ce3d6b007
Brian Foster <brian@brianfoster.net>
2024-09-25 13:50:07 -0400

    add error handling

--- src/App.js
+++ src/App.js
@@ -5,10 +5,12 @@ function App() {
   const [inputText, setInputText] = useState('');
   const [response, setResponse] = useState({ answer: '', links: [] });
   const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
 
   const handleSubmit = async () => {
     if (inputText !== '') {
       setLoading(true);  // Show the spinner
+      setError(false);  // Reset the error state
       try {
         const res = await fetch('https://bfoster-services.herokuapp.com/ai/assistant', {
           method: 'POST',
@@ -17,15 +19,32 @@ function App() {
           },
           body: JSON.stringify({ prompt: inputText }),
         });
-        const data = await res.json();
-        setResponse(data); // Update the response state
+
+        // const contentType = res.headers.get('content-type');
+        let data = await res.json();
+
+        if (isProperObject(data)) {
+          setResponse(data); // Update the response with the object
+        } else if (typeof data === 'string') {
+          setResponse({ answer: data, links: [] }); // Handle string response
+        } else {
+          setError(true);  
+          setResponse({ answer: "An unexpected error occurred.", links: [] });
+        }
+
       } catch (error) {
         console.error('Error:', error);
+        setResponse({ answer: "", links: [] });
+        setError(true);  // Show the error message
       } finally {
         setLoading(false);  // Hide the spinner
       }
     }
   };
+
+  function isProperObject(obj) {
+    return typeof obj === 'object' && obj !== null && !Array.isArray(obj);
+  }  
 
   // Function to format the answer by replacing newlines with <br/>
   const formatAnswer = (text) => {
@@ -35,6 +54,7 @@ function App() {
   return (
     <div className="App">
       <br />
+      {error && <div className="error">There was an error. Try again.</div>}
       <textarea
         style={{ width: '300px', margin: 'auto', textAlign: 'left' }}
         value={inputText}
@@ -53,7 +73,7 @@ function App() {
         <h3>Response:</h3>
         <p dangerouslySetInnerHTML={{ __html: formatAnswer(response.answer) }}></p>
         <h3>Links:</h3>
-        {response.links.map((item, index) => (
+        {response.links && response.links.map((item, index) => (
           <p key={index}>
             <a target="_blank" rel="noopener noreferrer" href={item}>{item}</a>
           </p>
