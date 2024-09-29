07e4ee2aec20faf59945e5f2b19a28c249620312
Brian Foster <brian@brianfoster.net>
2024-09-29 11:45:48 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,6 +4,10 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+if (window.location.protocol === 'http:' && window.location.hostname !== 'localhost') {
+  window.location.href = `https://${window.location.host}${window.location.pathname}${window.location.search}`;
+}
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
